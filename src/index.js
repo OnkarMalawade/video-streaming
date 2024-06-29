a60116d896b4775a7bd3f39bef2a681030eea5c9
Onkar Malawade <onkarmalawade@gmail.com>
2024-06-29 12:29:37 +0530

    Proper 1

---
+++ src/index.js
@@ -0,0 +1,35 @@
+//require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path : './env'
+})
+
+connectDB()
+
+/*
+import express from "express"
+const app = express()
+//function connectDB(){
+//
+//}
+;( async () => {
+    try {
+        await mangoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",() => {
+            console.error("Error : ", error);
+            throw error;
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    }
+    catch(error){
+        console.error("Error : " ,error)
+        throw error
+    }
+})()
+
+*/
