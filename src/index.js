03f109072654388f36c00a51d425252f01b697a2
Onkar Malawade <onkarmalawade@gmail.com>
2024-06-29 14:29:23 +0530

    errror solve

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 //require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
-
+import express from "express"
+const app = express()
 import connectDB from "./db/index.js"
 
 dotenv.config({
