d12cb621d86088bf6527737b435472f5ce671925
Onkar Malawade <onkarmalawade@gmail.com>
2024-06-29 14:28:06 +0530

    new codes added

--- src/index.js
+++ src/index.js
@@ -8,6 +8,18 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+    app.on("error",() => {
+        console.error("Error : ", error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at PORT : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB Error : ", err);
+})
 
 /*
 import express from "express"
